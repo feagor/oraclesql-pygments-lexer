b93f6de5faaa8913943997adcb1314714d7c3711
Feagor <36843125+feagor@users.noreply.github.com>
2025-12-02 15:23:59 +0500

    Update lexer.py

--- oraclesql/lexer.py
+++ oraclesql/lexer.py
@@ -1,16 +1,20 @@
 from pygments.lexers.sql import SqlLexer
-from pygments.token import Keyword, Name
+from pygments.token import Keyword, Name, Punctuation, Text, Whitespace, Comment
 
-# Импортируйте новые списки
+# 1. Импортируем новый список
 from oraclesql._oraclesql_builtins import (
-    ORACLE_KEYWORDS, ORACLE_PLSQL_KEYWORDS, ORACLE_DATATYPES, 
-    ORACLE_FUNCTIONS, ORACLE_PACKAGES
+    ORACLE_KEYWORDS, 
+    ORACLE_PLSQL_KEYWORDS, 
+    ORACLE_DATATYPES, 
+    ORACLE_PACKAGE_PREFIXES  # <-- Новый импорт
 )
 
-# Объединяем ключевые слова в один Set для скорости (O(1) поиск)
 ALL_KEYWORDS = set(ORACLE_KEYWORDS + ORACLE_PLSQL_KEYWORDS)
-# Типы данных тоже в Set
 ALL_TYPES = set(ORACLE_DATATYPES)
+
+# 2. Превращаем список префиксов в кортеж (tuple), 
+# так как startswith() принимает именно tuple.
+PACKAGE_PREFIXES_TUPLE = tuple(ORACLE_PACKAGE_PREFIXES)
 
 __all__ = ["OracleSQLLexer"]
 
@@ -21,18 +25,36 @@ class OracleSQLLexer(SqlLexer):
 
     def get_tokens_unprocessed(self, text):
         extra_content = [
-            (set(ORACLE_DATATYPES), Name.Builtin),   # Голубой (типы)
-            (set(ORACLE_FUNCTIONS), Name.Function),  # Обычно желтый или зеленый (функции)
-            (set(ORACLE_PACKAGES), Name.Namespace),  # Обычно курсив или другой цвет (пакеты)
-            (set(ORACLE_KEYWORDS + ORACLE_PLSQL_KEYWORDS), Keyword) # Фиолетовый
+            (ALL_TYPES, Name.Builtin),
+            (ALL_KEYWORDS, Keyword)
         ]
 
+        # 0=ничего, 1=пакет, 2=точка (ждем метод)
+        package_chain_state = 0
+
         for index, token, value in SqlLexer.get_tokens_unprocessed(self, text):
-            # Проверяем токены, которые SqlLexer посчитал "Именем" (Name)
+            # Пропуск пробелов/комментариев (не ломаем цепочку)
+            if token in Comment or token is Text or token is Whitespace:
+                yield index, token, value
+                continue
+            
+            val_upper = value.upper()
+
             if token is Name or token is Name.Builtin:
-                # ВАЖНО: Приводим к верхнему регистру для проверки!
-                val_upper = value.upper()
+                # А. Если ждем метод после точки (DBMS_OUTPUT . <PUT_LINE>)
+                if package_chain_state == 2:
+                    yield index, Name.Namespace, value
+                    package_chain_state = 0
+                    continue
+
+                # Б. Проверяем префикс пакета (DBMS_..., UTL_...)
+                # Используем кортеж для быстрой проверки всех вариантов разом
+                if val_upper.startswith(PACKAGE_PREFIXES_TUPLE):
+                    yield index, Name.Namespace, value
+                    package_chain_state = 1 # Запоминаем: это пакет
+                    continue
                 
+                # В. Обычные проверки (типы, кейворды)
                 found_match = False
                 for word_set, token_type in extra_content:
                     if val_upper in word_set:
@@ -40,9 +62,21 @@ class OracleSQLLexer(SqlLexer):
                         found_match = True
                         break
                 
-                if not found_match:
-                    yield index, token, value
-            else:
+                if found_match:
+                    package_chain_state = 0
+                    continue
+
+                # Г. Просто имя
+                package_chain_state = 0
                 yield index, token, value
 
-
+            elif token is Punctuation and value == '.':
+                if package_chain_state == 1:
+                    package_chain_state = 2 # Пакет был, теперь точка
+                else:
+                    package_chain_state = 0
+                yield index, token, value
+            
+            else:
+                package_chain_state = 0
+                yield index, token, value
