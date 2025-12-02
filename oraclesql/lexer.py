95e7beb3502ae04559ed6cb568a313aeedf55a0e
Feagor <36843125+feagor@users.noreply.github.com>
2025-12-02 13:56:13 +0500

    Update lexer.py
    add datatypes

--- oraclesql/lexer.py
+++ oraclesql/lexer.py
@@ -1,29 +1,42 @@
 from pygments.lexers.sql import SqlLexer
 from pygments.token import Keyword, Name
 
-from oraclesql._oraclesql_builtins import ORACLE_KEYWORDS, ORACLE_PLSQL_KEYWORDS
+from oraclesql._oraclesql_builtins import ORACLE_KEYWORDS, ORACLE_PLSQL_KEYWORDS, ORACLE_DATATYPES
 
-ORACLE_KEYWORDS += ORACLE_PLSQL_KEYWORDS
+# Объединяем ключевые слова в один Set для скорости (O(1) поиск)
+ALL_KEYWORDS = set(ORACLE_KEYWORDS + ORACLE_PLSQL_KEYWORDS)
+# Типы данных тоже в Set
+ALL_TYPES = set(ORACLE_DATATYPES)
 
 __all__ = ["OracleSQLLexer"]
 
-
-# TODO: Use a custom lexer for SQL
 class OracleSQLLexer(SqlLexer):
     name = "Oracle PL/SQL"
     aliases = ["oraclesql"]
     mimetypes = ["text/x-oracle-plsql"]
 
     def get_tokens_unprocessed(self, text):
-        extra_content = [(ORACLE_KEYWORDS, Keyword)]
+        # Определяем приоритет проверки:
+        # Сначала проверяем Типы, потом Ключевые слова
+        extra_content = [
+            (ALL_TYPES, Keyword.Type),  # Типы данных красим в Keyword.Type
+            (ALL_KEYWORDS, Keyword)     # Остальное в Keyword
+        ]
 
         for index, token, value in SqlLexer.get_tokens_unprocessed(self, text):
+            # Проверяем токены, которые SqlLexer посчитал "Именем" (Name)
             if token is Name or token is Name.Builtin:
-                for i in extra_content:
-                    if value in i[0]:
-                        yield index, i[1], value
+                # ВАЖНО: Приводим к верхнему регистру для проверки!
+                val_upper = value.upper()
+                
+                found_match = False
+                for word_set, token_type in extra_content:
+                    if val_upper in word_set:
+                        yield index, token_type, value
+                        found_match = True
                         break
-                else:
+                
+                if not found_match:
                     yield index, token, value
             else:
                 yield index, token, value
