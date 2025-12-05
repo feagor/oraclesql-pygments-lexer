372732e427ff3d4bffd9e1e23ac79a5b0ca28c14
Feagor <36843125+feagor@users.noreply.github.com>
2025-12-05 14:33:06 +0500

    Update lexer.py

--- oraclesql/lexer.py
+++ oraclesql/lexer.py
@@ -1,20 +1,25 @@
 from pygments.lexers.sql import SqlLexer
 from pygments.token import Keyword, Name, Punctuation, Text, Whitespace, Comment
 
-# 1. Импортируем новый список
+# 1. Импортируем новые, оптимизированные списки
 from oraclesql._oraclesql_builtins import (
-    ORACLE_KEYWORDS, 
-    ORACLE_PLSQL_KEYWORDS, 
-    ORACLE_DATATYPES, 
-    ORACLE_PACKAGE_PREFIXES  # <-- Новый импорт
+    ORACLE_KEYWORDS_CONTROL,
+    ORACLE_KEYWORDS_PLSQL,
+    ORACLE_KEYWORDS_TYPE,
+    ORACLE_BUILTINS_NAME,
+    ORACLE_SYS_PCK_PREF
 )
 
-ALL_KEYWORDS = set(ORACLE_KEYWORDS + ORACLE_PLSQL_KEYWORDS)
-ALL_TYPES = set(ORACLE_DATATYPES)
+# 2. Создаем объединенные наборы для эффективной проверки
+# Все управляющие ключевые слова (SQL + PL/SQL)
+ALL_CONTROL_KEYWORDS = set(ORACLE_KEYWORDS_CONTROL + ORACLE_KEYWORDS_PLSQL)
+# Все типы данных
+ALL_TYPES = set(ORACLE_KEYWORDS_TYPE)
+# Все встроенные функции/псевдо-колонки
+ALL_BUILTINS = set(ORACLE_BUILTINS_NAME)
 
-# 2. Превращаем список префиксов в кортеж (tuple), 
-# так как startswith() принимает именно tuple.
-PACKAGE_PREFIXES_TUPLE = tuple(ORACLE_PACKAGE_PREFIXES)
+# Превращаем список префиксов в кортеж (tuple) для быстрой проверки startswith()
+PACKAGE_PREFIXES_TUPLE = tuple(ORACLE_SYS_PCK_PREF)
 
 __all__ = ["OracleSQLLexer"]
 
@@ -24,16 +29,18 @@ class OracleSQLLexer(SqlLexer):
     mimetypes = ["text/x-oracle-plsql"]
 
     def get_tokens_unprocessed(self, text):
+        # Структура для проверки: (набор слов, тип токена)
         extra_content = [
-            (ALL_TYPES, Name.Builtin),
-            (ALL_KEYWORDS, Keyword)
+            (ALL_TYPES, Name.Builtin),      # Выделяем типы данных как встроенные имена
+            (ALL_BUILTINS, Name.Builtin),   # Выделяем встроенные функции/псевдо-колонки как встроенные имена
+            (ALL_CONTROL_KEYWORDS, Keyword.Control) # Выделяем управляющие слова (SQL/PLSQL) как ключевые слова
         ]
 
         # 0=ничего, 1=пакет, 2=точка (ждем метод)
         package_chain_state = 0
 
         for index, token, value in SqlLexer.get_tokens_unprocessed(self, text):
-            # Пропуск пробелов/комментариев (не ломаем цепочку)
+            # Skip spaces/comments (do not break the chain)
             if token in Comment or token is Text or token is Whitespace:
                 yield index, token, value
                 continue
@@ -41,20 +48,22 @@ class OracleSQLLexer(SqlLexer):
             val_upper = value.upper()
 
             if token is Name or token is Name.Builtin:
-                # А. Если ждем метод после точки (DBMS_OUTPUT . <PUT_LINE>)
+                # A. If waiting for a method after a dot (DBMS_OUTPUT.PUT_LINE)
                 if package_chain_state == 2:
-                    yield index, Name.Namespace, value
+                    # Выделяем метод пакета как Name.Function
+                    yield index, Name.Function, value
                     package_chain_state = 0
                     continue
 
-                # Б. Проверяем префикс пакета (DBMS_..., UTL_...)
-                # Используем кортеж для быстрой проверки всех вариантов разом
+                # B. Check for package prefix (DBMS_..., UTL_...)
+                # Use tuple for fast multi-option check
                 if val_upper.startswith(PACKAGE_PREFIXES_TUPLE):
+                    # Выделяем имя пакета как Name.Namespace
                     yield index, Name.Namespace, value
-                    package_chain_state = 1 # Запоминаем: это пакет
+                    package_chain_state = 1 # Mark: this is a package
                     continue
                 
-                # В. Обычные проверки (типы, кейворды)
+                # C. Check for regular keywords (types, builtins, control)
                 found_match = False
                 for word_set, token_type in extra_content:
                     if val_upper in word_set:
@@ -66,13 +75,13 @@ class OracleSQLLexer(SqlLexer):
                     package_chain_state = 0
                     continue
 
-                # Г. Просто имя
+                # D. Plain identifier (variable, column, table name)
                 package_chain_state = 0
                 yield index, token, value
 
             elif token is Punctuation and value == '.':
                 if package_chain_state == 1:
-                    package_chain_state = 2 # Пакет был, теперь точка
+                    package_chain_state = 2 # Package was seen, now we expect a method
                 else:
                     package_chain_state = 0
                 yield index, token, value
